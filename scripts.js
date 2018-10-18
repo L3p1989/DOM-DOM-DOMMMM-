82e7923bbb31c4be10ce91e709b9f66a20a3bc3a
Patrick Hogan <phogan110@live.com>
2018-10-18 17:28:55 -0600

    added comments

--- scripts.js
+++ scripts.js
@@ -16,19 +16,19 @@ document.body.appendChild(squareContainer);
 
 btn.addEventListener('click', function() {
     var squareDiv = document.createElement('div');
-    var testText = document.createTextNode('test')
+    // var testText = document.createTextNode('test');
 
     btnClick++;
 
-    squareContainer.appendChild(squareDiv);
+    squareContainer.appendChild(squareDiv);//this adds squareDiv to squareContainer
     // squareDiv.appendChild(testText)
 
     squareDiv.className = 'square'
 
-    squareDiv.id = ('s' + btnClick)
+    squareDiv.id = ('s' + btnClick);//this adds ids based on btnClick
 
-    var divText = document.createTextNode('s' + btnClick)
+    var divText = document.createTextNode('s' + btnClick);
 
-    squareDiv.appendChild(divText)
+    squareDiv.appendChild(divText);//this adds divText to squareDivs
 
 });
