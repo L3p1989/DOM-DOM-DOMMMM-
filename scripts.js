fb9b8377baca8011219e30a8dd6c955dc3eafb07
Patrick Hogan <phogan110@live.com>
2018-10-18 17:10:00 -0600

    added `btnClick` for counter to add `.square` ids

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ var btnText = document.createTextNode('Add Square');
 btn.appendChild(btnText);
 btnDiv.appendChild(btn);
 
+var btnClick = 0;
+
 var squareContainer = document.createElement('div')
 
 document.body.appendChild(squareContainer);
@@ -16,9 +18,12 @@ btn.addEventListener('click', function() {
     var squareDiv = document.createElement('div');
     var testText = document.createTextNode('test')
 
+    btnClick++;
+    console.log(btnClick)
+
     squareContainer.appendChild(squareDiv);
     // squareDiv.appendChild(testText)
 
     squareDiv.className = 'square'
-    
+
 });
