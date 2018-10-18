a8649b802a3502ab576ec4bcd10c390e0c537520
Patrick Hogan <phogan110@live.com>
2018-10-18 16:40:44 -0600

    added div to contain square divs

--- scripts.js
+++ scripts.js
@@ -4,8 +4,14 @@ var btnText = document.createTextNode('Add Square');
 btn.appendChild(btnText);
 document.body.appendChild(btn);
 
+var squareContainer = document.createElement('div')
+
+document.body.appendChild(squareContainer);
+
 btn.addEventListener('click', function() {
     var squareDiv = document.createElement('div');
 
-    document.body.appendChild(squareDiv)
+    squareContainer.appendChild(squareDiv);
+
+    
 });
